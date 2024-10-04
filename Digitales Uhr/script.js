3655c4e8bb06ec20c2f89d2e446b04f418a558dd
Omar <omar.mur@mail.ru>
2024-10-05 00:38:48 +0200

    Add code files

--- Digitales Uhr/script.js
+++ Digitales Uhr/script.js
@@ -1,11 +1,9 @@
 let hourElement = document.querySelector(".hour-show");
-
 let minuteElement = document.querySelector(".minute-show");
-
 let secondElement = document.querySelector(".second-show");
 
-function Time() {  
-
+function Time() {
+   
     // aktuelle Zeit bekommen
     
     let time = new Date();
@@ -16,7 +14,21 @@ function Time() {
     
     let second = time.getSeconds();
 
-}
+    
+    let hourGrad = (hour / 12) * 360;
 
-console.log(Time());
+    let minuteGrad = (minute / 60) * 360;
 
+    let secondGrad = (second / 60) * 360;
+
+    hourElement.style.transform = `rotate(${hourGrad}deg)`;
+
+    minuteElement.style.transform = `rotate(${minuteGrad}deg)`;
+    
+    secondElement.style.transform = `rotate(${secondGrad}deg)`;
+
+   }
+
+Time();
+
+setInterval(Time, 1000)
